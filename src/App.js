953849eb3e66a952e21339a516ea1d35d88dfdc2
lucsan <1104946+lucsan@users.noreply.github.com>
2019-07-23 14:30:58 +0100

    🛠 Updates App.js for better UX experience.

--- src/App.js
+++ src/App.js
@@ -2,94 +2,230 @@ import React, { Component} from "react";
 import ReactDOM from "react-dom";
 import {hot} from "react-hot-loader";
 import "./App.css";
-const rsp = require("spockscissorspaper")
 
-const list = ['paper', 'rock', 'scissors', 'lizard', 'spock', 'pen', 'sword']
-const emos = ['🧻', '💎', '✂', '🦎', '🖖', '🖋', '🗡']
+
+import "./bootstrap-4.3.1-dist/css/bootstrap-grid.min.css"
+import "./bootstrap-4.3.1-dist/css/bootstrap.min.css"
+
+const rsp = require("spockscissorspaper")
 const ssp = rsp.rspalator()
-let go = { w: '', p1: '', p2: '', m: '', e: '', go: false }
-let result = {}
-let pick = { p1: '', p2: '' }
+const list = [
+  { paper: '🧻' },
+  { rock: '💎' },
+  { scissors: '✂' },
+  { lizard: '🦎' },
+  { spock: '🖖' },
+  { pen: '🖋' },
+  { sword: '🗡' }
+]
+
+let play = { p1: '', p2: '', p1emo: '🎩', p2emo: '🥾', whosChoice: undefined }
+
 
 function App () {
-  let p1css = 'show'
-  let p2css = 'show'
-  if (pick.p1 == '' && pick.p2 == '') { p1css = 'show'; p2css = 'hide' }
-  if (pick.p1 != '' && pick.p2 == '') { p1css = 'hide'; p2css = 'show' }
-  if (pick.p1 != '' && pick.p2 != '') { p1css = 'show'; p2css = 'hide' }
-
-
+  play.whosChoice = playerOnesChoice
   return (
-    <div id="app">
-      <Winner />
-      <div id="one" className={p1css}>
-        <h3>Player One choose your weapon</h3>
-        <Buttons player="one" />
-      </div>
-
-      <div id="two" className={p2css}>
-        <h3>Player Two choose your weapon</h3>
-        <Buttons player="two" />
-      </div>
+    <div id="app" className="container">
+      <Preamble />
+      <Menu />
+      <Arena />
+      <Footnote />
     </div>
   )
 }
 
-class Welcome extends Component {
-  render() {
-    return (
-      <h2>Wecomes {this.props.name}</h2>
-    )
-  }
-}
+const Arena = () => { return <div id="arena" ><Welcome /></div> }
 
-function Winner(props) {
-  if (result.w == undefined) return <div></div>
-  return result.w == 'draw'?
-    <div><h2>{result.emo} {result.m}</h2></div>:
-    <div><h2>{result.emo} {result.w} Wins!</h2><div> {result.e} {result.m}</div></div>
-}
-
-function Buttons(props) {
-  let b = list.map((i, ix) => {
-    return (
-      <li key={i} >
-        {Button(props.player, i, ix)}
-      </li>
-    )
-  })
+const Welcome = () => {
+  play.p1 = ''
+  play.p2 = ''
+  play.whosChoice = playerOnesChoice
   return (
-    <ul id={props.player} className="button" >
-      {b}
-    </ul>
+    <div id="welcome">
+      <h3>welcome</h3>
+      <p>🖖 🧻 ✂</p>
+      <p>Spock Paper Scissors</p>
+      <p>💎 🦎 🖋</p>
+      <button onClick={e => renderArena(<PlayerOne />)} >🎲 Ready Player One?</button>
+      <p>A game like Rock Paper Scissors, but with more, so much more, like ...</p>
+      <p>Spock, a nimble lizard, and guest staring a pen, plus magically, a sword</p>
+      <p></p>
+      <p></p>
+    </div>
+  )
+
+}
+
+const Choices = (player) => {
+  return (
+    <div key={player.player}>
+      <ul key="choices"><Buttons player={player.player} /></ul>
+    </div>
   )
 }
 
-function Button(player, item, idx) {
+const Buttons = (props) => {
+  return list.map((i, ix) => {
+    const k = Object.keys(i)[0]
+    return (
+      <li key={k}>
+      <Button
+        choice={k}
+        emo={i[k]}
+        player={props.player}
+      />
+      </li>
+    )
+  })
+}
+
+const Button = (props) => {
   return (
     <button
-      onClick={ e => handlePick(item, player, e) }
-      title={item}
-      >
-      {emos[idx]}
+      key={props.choice}
+      id={props.choice}
+      title={props.choice}
+      onClick={e => play.whosChoice(props)}
+      className="player"
+    >
+      {props.emo}
     </button>
   )
 }
 
-function handlePick(i, p, e) {
-  if (p == 'one') { pick.p1 = i }
-  if (p == 'two') { pick.p2 = i }
-  if (pick.p1 != '' && pick.p2 != '') haveAGo()
-  ReactDOM.render(<App />, document.getElementById("root"))
+const playerOnesChoice = (props) => {
+  play.p1 = props.choice
+  play.whosChoice = playerTwosChoice
+  renderArena(<ReadyPlayerTwo />)
 }
 
-function haveAGo() {
-  result = ssp.haveAGo(pick.p1, pick.p2)
-  result.emo = result.w == 'draw'?
-    emos[list.indexOf(pick.p1)]:
-    emos[list.indexOf(result.w)]
-  pick.p1 = ''
-  pick.p2 = ''
+const playerTwosChoice = (props) => {
+  play.p2 = props.choice
+  play.whosChoice = playerOnesChoice
+  renderArena(<Outcome />)
 }
 
-export default hot(module)(App);
+const ReadyPlayerTwo = () => {
+  return (
+    <div id="ready-player-two">
+    <button>💪 Make the computor do it (challenge the computor)</button>
+    <div>Are you ready?</div>
+    <button onClick={e => renderArena(<PlayerTwo />)}>🎲 GO {play.p2emo} player Two</button>
+    </div>
+  )
+
+}
+
+const PlayerOne = () => {
+  return (
+    <div id="playerOne">
+      <h3>{play.p1emo} Player One</h3>
+      <Choices player="one" />
+    </div>
+  )
+}
+
+
+
+const PlayerTwo = () => {
+  return (
+    <div id="player-two">
+    <div>{play.p2emo} Play away Player two</div>
+    <div>
+      <Choices player="two" />
+    </div>
+
+    </div>
+
+
+  )
+}
+
+const Outcome = () => {
+  let result = ssp.haveAGo(play.p1, play.p2)
+  let winner = play.p1 == result.w? 'one': 'two'
+  let winnerEmo = play.p1 == result.w? play.p1emo: play.p2emo
+
+  let winningEmo = list.filter(i => Object.keys(i)[0] == result.w)
+console.log(winningEmo[0][result.w])
+  console.log(play,result)
+  return (
+    <div id="outcome">
+
+      <div>
+
+      <div>Outcome</div>
+      <div>winner {winnerEmo} {winningEmo[0][result.w]} </div>
+      <div>Player {winner} with {result.w}</div>
+      <div>{result.m}</div>
+      <div>{result.e}</div>
+      <div></div>
+      <div></div>
+      <div></div>
+
+      </div>
+
+      <button onClick={e => render(<PlayerOne />, "arena")}>
+        Go Again 🖖 💎 🗡
+      </button>
+
+    </div>
+  )
+}
+
+
+
+
+const Footnote = () => {
+  return <div id="footnote" >footnote</div>
+}
+
+const Preamble = () => {
+  return (
+    <div id="preamble" className="preamble" >
+      <h3><span>🖖 🧻 ✂</span> <span>Spock Paper Scissors</span> <span>💎 🦎 🖋</span></h3>
+    </div>
+  )
+}
+
+const Menu = () => {
+  return (
+    <div id="menu" className="menu" title="Mini menu Hamburger">
+      <h3 onClick={e => toggleMenu()}>🍔</h3>
+      <ul id="menu-list" className="flex-column hide" >
+        <li className="nav-item" onClick={e => renderArena(<Welcome />)} >Restart</li>
+        <li className="nav-item" onClick={e => renderArena(<Hamburger />)} >Have a 🍔 Hamburger</li>
+      </ul>
+    </div>
+  )
+}
+
+const toggleMenu = () => {
+  let ml = document.getElementById('menu-list')
+  if(ml.classList.contains('hide')) {
+    ml.classList.remove('hide')
+    ml.classList.add('nav')
+  } else {
+    ml.classList.add('hide')
+    ml.classList.remove('nav')
+  }
+}
+
+const Hamburger = () => {
+  return (
+    <div id="hamburger" >
+      <div>🍔</div>
+      <p>Here is a hamburger for you</p>
+      <button onClick={e => renderArena(<Welcome />)}>play Spock Sicssors Paper</button>
+    </div>
+  )
+}
+
+const renderApp = (cmpt) => { return render(cmpt, "app") }
+const renderArena = (cmpt) => { return render(cmpt, "arena") }
+
+const render = (cmpt, eId) => {
+  return ReactDOM.render(cmpt, document.getElementById(eId))
+}
+
+export default hot(module)(App)
